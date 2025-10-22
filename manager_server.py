815cca96235d68c1e03413fcc68be58d68c65d5a
Qwerty Jacob <jesusfcevallos@gmail.com>
2025-10-22 14:31:17 +0000

    advancements

--- manager_server.py
+++ manager_server.py
@@ -8,19 +8,35 @@ from preprocessing import HealthProbesBuffer
 from brain import Brain
 from communication import SMMetricsReporter
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+from confluent_kafka.admin import AdminClient
+from preprocessing import GenericBuffer
 import torch
 import string
 import random
 import time
+import pickle
 from confluent_kafka import Consumer, KafkaError
 import json
 import requests
+from modules import MLP
+from reporting import WeightsReporter, GlobalMetricsReporter
+from aggregation import federated_averaging, fed_yogi
+import sys
 
 MANAGER = "MANAGER"
 WANDBER = "WANDBER"
 SECURITY_MANAGER = "SECURITY_MANAGER"
+FEDERATED_LEARNING = "FEDERATED_LEARNING"
 HEALTHY = "HEALTHY"
 INFECTED = "INFECTED"
+
+aggregation_functions = {
+    "fedavg": federated_averaging,
+    "fedyogi": fed_yogi,
+    "fedprox": None,
+    "fedsgd": None
+    }
+
 
 class Wandber:
 
@@ -106,6 +122,7 @@ class SecurityManager:
         self.training_thread.start()
         self.resusbscription_thread.start()
         self.logger.info("Security manager started!")
+
 
     def get_status_from_dashboard(self, vehicle_name):
         url = f"http://{self.dashboard_endpoint}/vehicle-status"
@@ -213,16 +230,8 @@ class SecurityManager:
         }
         return Consumer(conf_cons)
 
+
     def deserialize_message(self, msg):
-        """
-        Deserialize the JSON-serialized data received from the Kafka Consumer.
-
-        Args:
-            msg (Message): The Kafka message object.
-
-        Returns:
-            dict or None: The deserialized Python dictionary if successful, otherwise None.
-        """
         try:
             # Decode the message and deserialize it into a Python dictionary
             message_value = json.loads(msg.value().decode('utf-8'))
@@ -375,6 +384,208 @@ class SecurityManager:
         self.logger.info(f"Security manager stopped.")
 
 
+class FederatedLearningManager:
+
+
+    def __init__(self, args):
+        logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=str(args['logging_level']).upper())
+        self.logger = logging.getLogger(FEDERATED_LEARNING)
+        self.logger.debug("Initializing federated learning manager")
+        self.mode = args['mode']
+        self.probe_metrics = args['probe_metrics']
+        self.aggregation_interval_secs = args['aggregation_interval_seconds']
+        if self.mode == 'SW':
+            self.input_dim = args['input_dim'] + len(self.probe_metrics)
+            self.output_dim = 4
+
+        self.global_model = MLP(input_dim=self.input_dim, output_dim=self.output_dim, **args)
+        self.global_model.initialize_weights(args['initialization_strategy'])
+        self.logger.info(f"Global model initialized using {args['initialization_strategy']} initialization.")
+
+        self.admin_client = AdminClient({'bootstrap.servers': args['kafka_broker_url']})
+        self.vehicle_weights_topics = self.check_vehicle_weights_topics(args)
+
+        self.weights_buffer = self.create_weights_buffer(**args)
+        self.weights_reporter = WeightsReporter(self.logger, **args)
+        self.global_metrics_reporter = GlobalMetricsReporter(self.logger, **args)
+
+        self.eval_feats, self.eval_labels = self.load_eval_df(**args)
+        self.logger.info(f"Starting FL with {len(self.vehicle_weights_topics)} in {self.mode} mode" + \
+                         f" for vehicles: {self.vehicle_weights_topics}, probing metrics: {self.probe_metrics}")
+        self.stop_threads = False
+
+        self.consuming_thread = threading.Thread(
+            target=self.consume_weights_data, 
+            kwargs=args, 
+            daemon=True
+            )
+        
+        self.consuming_thread.start()
+
+        if self.aggregation_interval_secs > 0:
+            # create a thread to aggregate the weights each aggregation_interval_secs:
+            self.aggregation_thread = threading.Thread(
+                target=self.aggregate_weights_periodically, 
+                kwargs=args)
+            self.aggregation_thread.start()
+    
+
+    def graceful_shutdown(self):
+        self.stop_threads = True
+        if self.consuming_thread:
+            self.consuming_thread.join()
+        if self.aggregation_thread:
+            self.aggregation_thread.join()
+        self.logger.info(f"Federated learning manager stopped.")
+
+
+    def aggregate_weights_periodically(self, **kwargs):
+        while not self.stop_threads:
+            time.sleep(kwargs.get('aggregation_interval_secs'))
+            self.aggregate_weights(**kwargs)
+            if self.mode == 'OF':
+                self.evaluate_new_model()
+
+
+    def evaluate_new_model(self):
+        epoch_accuracy = 0
+        epoch_precision = 0
+        epoch_recall = 0
+        epoch_f1 = 0
+        self.logger.debug("Evaluating new model...")
+        self.global_model.eval()
+        len_eval_ds = len(self.eval_feats)
+        with torch.no_grad():
+            for batch_feats, batch_labels in zip(self.eval_feats, self.eval_labels): 
+                batch_preds = self.global_model(batch_feats)
+                batch_preds = (batch_preds > 0.5).float()
+                epoch_accuracy += accuracy_score(batch_labels, batch_preds)
+                epoch_precision += precision_score(batch_labels, batch_preds, zero_division=0)
+                epoch_recall += recall_score(batch_labels, batch_preds, zero_division=0)
+                epoch_f1 += f1_score(batch_labels, batch_preds, zero_division=0)
+            
+        epoch_accuracy /= len_eval_ds
+        epoch_precision /= len_eval_ds
+        epoch_recall /= len_eval_ds
+        epoch_f1 /= len_eval_ds
+        self.logger.info(f"Eval Accuracy: {epoch_accuracy}, Precision: {epoch_precision}, Recall: {epoch_recall}, F1: {epoch_f1}")
+        self.global_metrics_reporter.report_metrics({'accuracy': epoch_accuracy, 'precision': epoch_precision, 'recall': epoch_recall, 'f1': epoch_f1})
+        
+
+    def consume_weights_data(self, **kwargs):
+
+        consumer = self.create_consumer(**kwargs)
+
+        consumer.subscribe(self.vehicle_weights_topics)
+        self.logger.info(f"will start consuming {self.vehicle_weights_topics}")
+
+        try:
+            while not self.stop_threads:
+                msg = consumer.poll(5.0)  
+                if msg is None:
+                    continue
+                if msg.error():
+                    if msg.error().code() == KafkaError._PARTITION_EOF:
+                        self.logger.info(f"End of partition reached for {msg.topic()}")
+                    else:
+                        self.logger.error(f"consumer error: {msg.error()}")
+                    continue
+
+                deserialized_data = self.deserialize_message(msg)
+                if deserialized_data:
+                    self.process_message(msg.topic(), deserialized_data, **kwargs)
+
+        except KeyboardInterrupt:
+            self.logger.info(f" Consumer interrupted by user.")
+        except Exception as e:
+            self.logger.error(f" Error in consumer: {e}")
+        finally:
+            consumer.close()
+            self.logger.info(f" Consumer closed.")
+
+
+    def create_consumer(self, **kwargs):
+        # Kafka consumer configuration
+        conf_cons = {
+            'bootstrap.servers': kwargs.get('kafka_broker_url'),  # Kafka broker URL
+            'group.id': kwargs.get('kafka_consumer_group_id'),  # Consumer group ID
+            'auto.offset.reset': kwargs.get('kafka_auto_offset_reset')
+        }
+        return Consumer(conf_cons)
+
+
+    def deserialize_message(self, msg):
+
+        try:
+            # Decode the message and deserialize it into a Python dictionary
+            message_value = pickle.loads(msg.value())
+            self.logger.debug(f"received message from topic [{msg.topic()}]")
+            return message_value
+        except json.JSONDecodeError as e:
+            self.logger.error(f"Error deserializing message: {e}")
+            return None
+
+
+    def aggregate_weights(self, **kwargs):
+
+        # check if we have at least one element in each buffer:
+        if all([len(buffer) > 0 for buffer in self.weights_buffer.values()]):
+            self.logger.debug(f"Aggregating the weights from {len(self.weights_buffer)} vehicles.")
+            aggregation_function = aggregation_functions[kwargs.get('aggregation_strategy')]
+
+            for buffer in self.weights_buffer.values():
+                candidate_state_dict = buffer.get()
+                if any(torch.isnan(param).any() for param in candidate_state_dict.values()):
+                    self.logger.error(f"Candidate weights from {buffer.label} contain NaNs. Skipping update.")
+                    # buffer.pop()
+                    return
+            
+            if aggregation_function is federated_averaging:
+                aggregated_state_dict = aggregation_function(
+                    self.global_model.state_dict(), 
+                    [buffer.get() for buffer in self.weights_buffer.values()])
+            else:
+                aggregated_state_dict = aggregation_function(
+                    self.global_model.state_dict(), 
+                    [buffer.get() for buffer in self.weights_buffer.values()], **kwargs)
+            
+            # Check if the aggregated state dict has no NaNs
+            if any(torch.isnan(param).any() for param in aggregated_state_dict.values()):
+                self.logger.error("Aggregated state dict contains NaNs. Skipping update.")
+                return
+            # pop the oldest element from each one of the buffers
+            for buffer in self.weights_buffer.values():
+                buffer.pop()
+            self.global_model.load_state_dict(aggregated_state_dict)
+            self.weights_reporter.push_weights(self.global_model.state_dict())
+        else:
+            self.logger.debug(f"Waiting for more data to aggregate the weights.")
+
+
+
+    def process_message(self, topic, msg, **kwargs):
+
+        self.weights_buffer[topic].add(msg)
+
+        if kwargs.get('aggregation_interval_secs') == 0:
+            self.aggregate_weights(**kwargs)
+
+
+    def create_weights_buffer(self, **kwargs):
+        weights_buffer = {}
+        for topic in self.vehicle_weights_topics:
+            weights_buffer[topic] = GenericBuffer(size=kwargs.get('weights_buffer_size', 3), label=topic)
+        return weights_buffer
+
+
+    def check_vehicle_weights_topics(self, args):
+        existing_topics = self.admin_client.list_topics(timeout=10).topics.keys()
+        # get all topics ending with "_weights"
+        vehicle_topics = [topic for topic in existing_topics if topic.endswith("_weights") and topic != "global_weights"]
+        self.logger.debug("Found the following vehicle topics: %s", vehicle_topics)
+        return vehicle_topics
+
+
 class ManagerAPI(ContainerAPI):
 
     def __init__(self, port: int = 5000):
@@ -385,6 +596,7 @@ class ManagerAPI(ContainerAPI):
             )
         self.wandber_instance = None
         self.sm_instance = None
+        self.fl_instance = None
         
     
     def handle_command(self, command, params):
@@ -410,14 +622,27 @@ class ManagerAPI(ContainerAPI):
             if self.sm_instance is None:
                 return "Security manager is not running"
             self.sm_instance.mitigation = True
+            self.logger.info(f"Mitigation is {self.sm_instance.mitigation}")
             return "Succesfully started mitigation"
         elif command == "stop_mitigation":
             if self.sm_instance is None:
                 return "Security manager is not running"
+            self.logger.info(f"Mitigation is {self.sm_instance.mitigation}")
             self.sm_instance.mitigation = False
             return "Succesfully stopped mitigation"
+        elif command == "start_federated_learning":
+            if self.fl_instance is None:
+                self.fl_instance = FederatedLearningManager(params)
+            return "Succesfully started federated learning"
+        elif command == "stop_federated_learning":
+            if self.fl_instance is not None:
+                self.fl_instance.graceful_shutdown()
+                return "Succesfully stopped federated learning"
+            else:
+                return "Federated learning is not running"
         else:
             return "Unrecognized command"
+
 
 def signal_handler(sig, frame):
     global api
@@ -425,6 +650,11 @@ def signal_handler(sig, frame):
     print(f"{MANAGER}: Received signal {sig}. Gracefully stopping wandb and its consumer threads.")
     if api.wandber_instance is not None:
         api.wandber_instance.graceful_shutdown()
+    if api.sm_instance is not None:
+        api.sm_instance.graceful_shutdown()
+    if api.fl_instance is not None:
+        api.fl_instance.graceful_shutdown()
+    sys.exit(0)
 
 
 def main():
